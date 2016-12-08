8bbf59c79109b7f5169e265f34490276c479cfa6
Dana <craciundaniela19@gmail.com>
2016-12-08 12:00:38 +0200

    reverting to orig

--- src/com/dana/Main.java
+++ src/com/dana/Main.java
@@ -20,8 +20,6 @@ import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
-        // Modified this comment a few hours ago
-        // etc
         AccountManager manager = new AccountManager();
 
         manager.addAccount("Maria", 9000);
